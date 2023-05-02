1b95e6e7c40b243771b37c02cea9708a731d2347
Aakarsh Verma <myselfmrv@gmail.com>
2023-05-02 08:09:50 +0530

    SERVER: Added model for user

---
+++ server/src/models/user.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      indexed: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+export default User;
